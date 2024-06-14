e9cb066b2196119f74ee86f64b2a25199678fe2f
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-14 23:16:40 +0200

    Base scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import AppHeader from './components/AppHeader.vue';
+
+</script>
+
+<template>
+
+  <AppHeader/>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/mixin' as *;
+@use './styles/partials/variants' as *;
+
+// font-google Roboto Slab
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap');
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+
+
+</script>
+
+<template>
+
+  <header>
+    <h1>Vite-boolflix</h1>
+  </header>
+
+</template>
+
+<style scoped>
+h1 {
+  color: purple;
+  background-color: brown;
+}
+</style>
