e2741ff7abbfa7a7449c579913eecab65d8932f3
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-15 15:16:22 +0200

    Creare struttura per inserimento immagini

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,20 +6,10 @@ export default {
   data() {
     return {
       films: [
-        {
-          titolo: 'Il re leone',
-          titoloOriginale: 'The lion king',
-          lingua: 'Italiano',
-          voto: '9.5326871',
-        },
-        {
-          titolo: 'Madagascar',
-          titoloOriginale: 'Madagascar',
-          lingua: 'Italiano',
-          voto: '7.824871',
-        },
+        { titolo: 'Il re leone', titoloOriginale: 'The lion king', lingua: 'Italiano', voto: '9.5326871', img: 'https://pad.mymovies.it/filmclub/2017/04/249/locandina.jpg'},
+        { titolo: 'Madagascar', titoloOriginale: 'Madagascar', lingua: 'Italiano', voto: '7.824871', img: 'https://pad.mymovies.it/filmclub/2005/05/064/locandinapg1.jpg'},
       ],
-      index: 0,
+      item: Object
     }
   },
 
@@ -33,7 +23,8 @@ export default {
   // of a component's lifecycle.
   // This function will be called when the component is mounted.
   mounted() {
-    console.log(this.films.titolo)
+    console.log(this.films[0].titolo)
+    console.log(this.films[1].titolo)
   }
 }
 
@@ -44,38 +35,56 @@ export default {
   <main>
 
     <section>
-      <input type="text" name="searchbar" id="searchbar">
-      <button>Invia</button>
-    </section>
 
-    <div>
-      <ul v-for="(films, index) in films" :key="index" v-show="films">
-        <li>Titolo: {{ films.titolo }}</li>
-        <li>Titolo Originale: {{ films.titoloOriginale }}</li>
-        <li>Lingua: {{ films.lingua }}</li>
-        <li>Voto: {{ parseInt(Number(films.voto)) }}</li>
-      </ul>
-    </div>
+      <div>
+        <input type="text" name="searchbar" id="searchbar">
+        <button>Invia</button>
+      </div>
+
+      <div>
+        <ul v-for="(films, index) in films" :key="index">
+          <li>
+            <img :src="films.img" :alt="films.titolo">
+          </li>
+          <li>Titolo: {{ films.titolo }}</li>
+          <li>Titolo Originale: {{ films.titoloOriginale }}</li>
+          <li>Lingua: {{ films.lingua }}</li>
+          <li>Voto: {{ parseInt(Number(films.voto)) }}</li>
+        </ul>
+      </div>
+
+    </section>
 
   </main>
 
 </template>
 
 <style lang="scss" scoped>
-@use '../styles/general.scss' as *;
 @use '../styles/partials/mixin' as *;
-@use '../styles/partials/variants' as *;
 
-section{
-  @include margin-min;
-}
+section {
+  div {
+    @include margin-min;
+  }
+  ul {
+    @include margin-min;
+    width: 25rem;
+    padding: .5rem;
+    border: 1px solid black;
 
-ul{
-  @include margin-min;
+    li {
+      list-style: none;
+      margin-bottom: .5rem;
 
-  li{
-    list-style: none
+      img {
+        width: 25rem;
+      }
+    }
+    li:last-of-type {
+      margin-bottom: 0;
+    }
   }
 }
+
 
 </style>
