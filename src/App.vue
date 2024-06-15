fa319d4bfe966c92bc0a6f8f7e45ecf80ee6afd5
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-15 13:56:29 +0200

    Realizzare componente AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,14 @@
 <script setup>
 
 import AppHeader from './components/AppHeader.vue';
-
-</script>
-
-<script>
-
-export default {
-  // Properties returned from data() become reactive state
-  // and will be exposed on `this`.
-  data() {
-    return {
-      films: [
-        {
-          titolo: 'Il re leone',
-          titoloOriginale: 'The lion king',
-          lingua: 'Italiano',
-          voto: '7.5326871',
-        }
-      ],
-      index: 0,
-    }
-  },
-
-  // Methods are functions that mutate state and trigger updates.
-  // They can be bound as event handlers in templates.
-  methods: {
-
-  },
-
-  // Lifecycle hooks are called at different stages
-  // of a component's lifecycle.
-  // This function will be called when the component is mounted.
-  mounted() {
-    console.log(this.films.titolo)
-  }
-}
+import AppMain from './components/AppMain.vue';
 
 </script>
 
 <template>
 
   <AppHeader/>
-  <main>
-
-    <section>
-      <input type="text" name="searchbar" id="searchbar">
-      <button>Invia</button>
-    </section>
-
-    <div>
-      <ul v-if="films" >
-        <li>Titolo: {{ films[index].titolo }}</li>
-        <li>Titolo Originale: {{ films[index].titoloOriginale }}</li>
-        <li>Lingua: {{ films[index].lingua }}</li>
-        <li>Voto: {{ parseInt(Number(films[index].voto)) }}</li>
-      </ul>
-    </div>
-
-  </main>
+  <AppMain/>
 
 </template>
 
@@ -66,16 +16,4 @@ export default {
 @use './styles/general.scss';
 @use './styles/partials/mixin';
 @use './styles/partials/variants';
-
-section{
-  margin-left: 1.5rem;
-}
-
-ul{
-  margin-left: 2rem;
-
-  li{
-    list-style: none
-  }
-}
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,79 @@
+<script>
+
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      films: [
+        {
+          titolo: 'Il re leone',
+          titoloOriginale: 'The lion king',
+          lingua: 'Italiano',
+          voto: '9.5326871',
+        },
+        {
+          titolo: 'Madagascar',
+          titoloOriginale: 'Madagascar',
+          lingua: 'Italiano',
+          voto: '7.824871',
+        },
+      ],
+      index: 0,
+    }
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    console.log(this.films.titolo)
+  }
+}
+
+</script>
+
+<template>
+
+  <main>
+
+    <section>
+      <input type="text" name="searchbar" id="searchbar">
+      <button>Invia</button>
+    </section>
+
+    <div>
+      <ul v-for="(films, index) in films" :key="index" v-show="films">
+        <li>Titolo: {{ films.titolo }}</li>
+        <li>Titolo Originale: {{ films.titoloOriginale }}</li>
+        <li>Lingua: {{ films.lingua }}</li>
+        <li>Voto: {{ parseInt(Number(films.voto)) }}</li>
+      </ul>
+    </div>
+
+  </main>
+
+</template>
+
+<style lang="scss" scoped>
+
+section{
+  margin-left: 1.5rem;
+}
+
+ul{
+  margin-left: 2rem;
+  margin-bottom: 1rem;
+
+  li{
+    list-style: none
+  }
+}
+
+</style>
