51bda676aa202c79859c2eb87fb3480f2a55dae3
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-17 22:22:12 +0200

    Metto in Commit tutto il codice perché richiede un Ulteriore revisione

--- src/store.js
+++ src/store.js
@@ -1,6 +1,23 @@
-import { reactive } from 'vue';
+// import { reactive } from 'vue';
+// import axios from 'axios';
 
-export const store = reactive({
-    movieList: [],
-    seriesList: [],
-});
+// export const store = reactive ({
+//     apiUrl : "https://api.themoviedb.org/3/discover/movie?",
+//     apiKey : "api_key=68ea9ce68b5006f086ea95c89dbfabe9",
+//     lang : "it-IT",
+//     movieList: {},
+//     queryRicerca : "",
+//     categoryRicerca : {},
+
+//     isLoading : false,
+//     flags : [ "ar", "ca", "cs", "da", "de", "en", "es", "fr", "hi", "id", "is", "it", "ja", "ko", "mr", "ms", "nl", "no", "pl", "pt", "ro", "ru", "sk", "sv", "te", "th", "tl", "tr", "zh", ],
+//     getApi() {
+//       axios.categoryRicerca()
+//       .then((response) => {
+//         this.categoryRicerca = response.data.results;
+//         console.log(categoryRicerca)
+//       });
+//     },
+//     isSearching : false,
+//   });
+  
