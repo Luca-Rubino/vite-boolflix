dc24c2f83f72af294216a9853e4e4157588b861b
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-18 22:34:25 +0200

    Trasferire lista titoli discover in un componente

---
+++ src/components/listaTitoli.vue
@@ -0,0 +1,87 @@
+<script>
+
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+    data() {
+        return {
+            
+        }
+    },
+
+    props:{
+      filmLista: {
+            
+        },
+    },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+    methods: {},
+    created() {},
+
+    // Lifecycle hooks are called at different stages
+    // of a component's lifecycle.
+    // This function will be called when the component is mounted.
+    mounted() {
+      console.log(this.filmLista)
+    }
+}
+
+</script>
+
+<template>
+      <div>
+        <ul v-for="(filmLista, page) in filmLista" :key="page">
+          <li>
+            <img :src="`https://image.tmdb.org/t/p/original${filmLista.poster_path}`" :alt="filmLista.title">
+          </li>
+          <li>Titolo: {{ filmLista.title }}</li>
+          <li>Titolo Originale: {{ filmLista.original_title }}</li>
+          <li>Lingua: {{ filmLista.original_language }}</li>
+          <li>Voto: {{ parseInt(Number(filmLista.vote_average)) }}</li>
+        </ul>
+      </div>
+</template>
+
+<style lang="scss">
+@use '../styles/partials/mixin' as *;
+
+  section {
+    div {
+      @include margin-min;
+      display: flex;
+      flex-wrap: wrap;
+
+      label {
+        margin-right: .3rem
+      }
+    }
+    ul {
+      @include margin-min;
+      width: 25rem;
+      padding: .5rem;
+      border: 2px solid rgb(215, 198, 6);
+      border-radius: 25px;
+
+      li {
+        list-style: none;
+        margin-bottom: .5rem;
+
+        img {
+          width: 25rem;
+          height: 35rem;
+          border-radius: 25px;
+          position: relative;
+        }
+        img:active {
+          bottom: 1px;
+          top: 1px;
+        }
+      }
+      li:last-of-type {
+        margin-bottom: 0;
+      }
+    }
+  }
+</style>
